620cf43a0cc4f4c9a73858083785180074771a90
Young <allen@gmail.com>
2021-07-04 19:21:49 +0800

    优化 manualChunks 逻辑

--- vite.config.js
+++ vite.config.js
@@ -14,22 +14,20 @@ export default defineConfig({
       '@components': resolve(__dirname, './src/components'),
       '@pages': resolve(__dirname, './src/pages'),
       '@store': resolve(__dirname, './src/store'),
-      '@utils': resolve(__dirname, './src/utils')
-    }
+      '@utils': resolve(__dirname, './src/utils'),
+    },
   },
   build: {
     rollupOptions: {
       output: {
-        //- Use build.rollupOptions.output.manualChunks to improve chunking: https://rollupjs.org/guide/en/#outputmanualchunks
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('/quasar/')) {
-              return 'quasar'
-            }
-            return 'vender'
+          if (id.includes('/node_modules/')) {
+            const modules = ['quasar']
+            const chunk = modules.find((module) => id.includes(`/node_modules/${module}`))
+            return chunk ? `vendor-${chunk}` : 'vendor'
           }
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
 })
